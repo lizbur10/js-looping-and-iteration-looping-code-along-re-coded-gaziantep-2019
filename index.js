d03214b6f4ef82d292a0d61758d6d60a56760597
Liz Burton <liz.burton147@gmail.com>
2019-09-07 01:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function writeCards(namesArray, event) {
   const messages = [];
-  for (i = 0; i < namesArray.length; i++) {
+  for (let i = 0; i < namesArray.length; i++) {
     messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift.`);
   }
   return messages;

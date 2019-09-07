7722936a28d9a8ae32d5fa77b27eb746d3af9831
Liz Burton <liz.burton147@gmail.com>
2019-09-07 00:54:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solutions in this file
-// function writeCards(namesArray, event) {
-//   for (i = 0; i < namesArray.length; i++) {
-//     console.log(namesArray[i]);
-//   }
-// }
+function writeCards(namesArray, event) {
+  for (i = 0; i < namesArray.length; i++) {
+    console.log(namesArray[i]);
+  }
+}
+
